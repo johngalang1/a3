8d60f88c33a15290c1b6a5d38f587ad205da13cb
johngalang1 <113706696+johngalang1@users.noreply.github.com>
2023-04-05 18:16:20 -0600

    Update a3help.cpp

--- a3help.cpp
+++ a3help.cpp
@@ -1,120 +1,58 @@
-void process_ingredients(ifstream &in, Grocery_Item_Ptr &head) 
-{   
-    Grocery_Item_Ptr newNode = NULL; 
-    Grocery_Item_Ptr temp1 = NULL;
+void GroceryList::insertItem(const NAME aName, int quantity){
+// creates a node for inserting items
 
-    int amount;
-    char food[SIZE + 1]; 
-    bool success = true;
-    int counter = 0;
+    GroceryPtr newNode = NULL; // initializes a node poitner
 
-    // creates an empty Node to pinpoint the start
+    GroceryPtr headPtr = &head; // sets the adress of head
+    GroceryPtr tailPtr = &tail; // sets the adress of tail
 
-    newNode = new Grocery_Item; //Creates new Node
-    newNode->quantity = 0; 
-    strncpy(newNode->name, "\0", SIZE);  
-    head = newNode; //head is the startpoint
-    temp1 = newNode; //temp is also the startpoint
+    newNode = new GroceryNode; // creates a new node
+    newNode->quantity = quantity; // stores the quantity
+    strncpy(newNode->name, aName, MAX_ITEM_NAME_LEN); // stores the name
 
-    // Get all information for a node
-    read_input_line(in, amount, food, success); 
+    if (head.next == &tail && tail.prev == &head){ // if the item is the first entry to place in the list
+        head.next = newNode;
+        tail.prev = newNode;
 
-    while(success){ 
-            
-        newNode = new Grocery_Item; // Creates new Node
-        newNode->quantity = amount;  // Inserts the amount
-        strncpy(newNode->name, food, SIZE); //Inserts the name
-        temp1->next = newNode; // points towards the new Node, creating a link
-        temp1 = newNode; // temp1 is now situtated at the new Node
+        newNode->prev = &head;
+        newNode->next = &tail;
 
-        counter++; // increments the amount of nodes1`    
+    } else // if there is already an item in the list
+    {
+        if(strncmp(newNode->name, head.next->name, MAX_ITEM_NAME_LEN) < 0){ // if the new item is less than the node next to the head
+            head.next->prev = newNode;
 
-        read_input_line(in, amount, food, success);
-    }
+            newNode->next = head.next;
+            newNode->prev = &head;
 
-    newNode->next = NULL;
+            head.next = newNode;
+        } else // if the item is not less than the node next to the head
+        {
+            headPtr = headPtr->next; // points at the next node from the dummy head node
 
-    insert_item(head);
-    
-    return;
+            while(headPtr != tailPtr && (strncmp(newNode->name, headPtr->name, MAX_ITEM_NAME_LEN) >= 0))
 
-}
+            {// loop through the list to find the first iteration where the item is less than the next item
+                headPtr = headPtr->next;
+            }
 
+            if (headPtr == tailPtr){
 
-void insert_item(Grocery_Item_Ptr &head){ 
+                newNode->prev = tail.prev;
+                tail.prev->next = newNode;
 
-    combine_items(head); 
-    organize_list(head);
+                tail.prev = newNode; // set the previous dummy node to the new node
+                newNode->next = &tail;
 
-    return;
-}
+            } else
+            {
+                newNode->prev = headPtr->prev;
+                headPtr->prev->next = newNode;
 
-void combine_items(Grocery_Item_Ptr &head){ 
-    int compare;
-    
-    Grocery_Item_Ptr temp3 = head; // temp3 is set to our head
-
-    while (temp3!= NULL){ // until the link is done
-
-        Grocery_Item_Ptr temp1 = temp3; // set temp1 to temp3
-        Grocery_Item_Ptr temp2 = temp3->next; // set temp2 to be the next after temp
-
-        while(temp2 != NULL){ //temp2 will go through each node in the list until it reaches a NULL
-
-            compare = strncmp(temp3->name, temp2->name, SIZE); // checks if they're equal
-
-            if (compare == 0){ 
-
-                temp3->quantity += temp2->quantity;  // increment quantity
-                temp1->next = temp2->next; //the node will connect to the node after the next node
-                delete temp2; // then we delete the next node 
-                temp2 = temp1->next;  //then we set this next node to be the next of our temp1 node
-
-            } else{ 
-
-                temp1 = temp2;     // temp1 is now at temp2
-                temp2 = temp2->next; // temp2 will become the upcoming node
+                headPtr->prev = newNode;
+                newNode->next = headPtr;
             }
         }
-        temp3 = temp3->next; // iterate to the next node if there are any duplicates throughout the node
     }
-    return;
 }
 
-void organize_list(Grocery_Item_Ptr &head){ 
-    int counter = 0;
-    Grocery_Item_Ptr current = head; 
-    while(current!= NULL){ 
-        counter++; 
-        current = current->next;
-    }
-    Grocery_Item_Ptr temp1 = NULL, temp2 = NULL, temp3 = NULL; 
-    int compare;
-
-    for (int i=0; i < counter - 1; i++){   
-
-        temp3 = head; 
-        temp1 = temp3->next; 
-        temp2 = temp1->next;
-
-        for(int j = 0; j < counter - 2; j++){  
-
-            compare = strncmp(temp1->name,temp2->name,SIZE); 
-
-            if (compare < 0){ // If there the character is less than the next one, compare the next pair of nodes
-                temp1 = temp1->next; 
-                temp2 = temp2->next;
-            } else {  //If there character is greater than the next one
-                temp1->next = temp2->next; // Set the next node of temp1 to the next node of temp2
-                temp2->next = temp3->next; // Set the next node of temp2 to the next node after the head
-                temp3->next = temp2; // set the next node of the head towards temp2
-
-                head->next = temp3->next; // now actually set head
-
-                temp2 = temp1->next; // make temp2 to compare for the next node
-
-            } 
-        }
-    }
-    return;
-}
